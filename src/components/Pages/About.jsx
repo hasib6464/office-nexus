b6701d485ef554c2fba91c3c65fed6835ea07c1a
hasib6464 <hasibmahmud6464@gmail.com>
2025-06-01 12:45:20 +0600

    layout complete

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import about1 from "../../assets/aboutimg.jpg";
 import about2 from "../../assets/aboutimg2.jpg";
 import about3 from "../../assets/aboutimg3.jpg";
+import metting from "../../assets/meeting1.jpg";
 
 const About = () => {
   return (
-    <div>
+    <div className="">
       {/* title */}
       <div className="w-1/2 mx-auto my-10">
         <div className="flex flex-col justify-center items-center py-10 mb-5 shadow space-y-5 bg-base-200">
@@ -41,7 +42,7 @@ const About = () => {
       </div>
 
       {/* work hub */}
-      <div className="flex my-20 border py-20 bg-slate-100">
+      <div className="flex my-30  px-3 py-20 bg-slate-100">
         <div className="">
           <h1 className="text-4xl font-bold pb-10">
             Our Story: Crafting the WorkHub Experience
@@ -63,6 +64,69 @@ const About = () => {
             design principles resonated with forward-thinking companies and
             freelancers alike.
           </p>
+        </div>
+      </div>
+
+      {/* mission and vission */}
+      <div className="flex justify-between my-20">
+        <div className=" w-[457px] h[559px]">
+          <img className="rounded-lg" src={metting}></img>
+        </div>
+        <div className="w-[580px] flex flex-col justify-center space-y-10">
+          <h1 className="text-4xl font-bold">
+            Mission and Vision: Enabling Empowered Work Environments
+          </h1>
+          <p className="text-lg font-medium">
+            Our mission and vision revolve around the creation of work
+            environments that empower and inspire. At the core, we aim to
+            transform the traditional workspace into a dynamic, flexible area.
+          </p>
+        </div>
+      </div>
+
+      {/*  Quality commitment*/}
+      <div className="flex justify-between my-10">
+        <div className="w-[580px] flex flex-col justify-center space-y-10">
+          <h1 className="text-4xl font-bold">
+            Quality Commitment: Upgrading Your Workspace
+          </h1>
+          <p className="text-lg font-medium">
+            Our commitment to quality is at the forefront of everything we do,
+            aiming to elevate every aspect of your workspace experience.
+          </p>
+          <div className="flex space-x-10">
+            <div>
+              <p className="text-4xl font-bold text-blue-800">5+</p>
+              <p className="text-lg">Years in Operation</p>
+            </div>
+            <div>
+              <p className="text-4xl font-bold text-blue-800">6+</p>
+              <p className="text-lg">Locations</p>
+            </div>
+            <div>
+              <p className="text-4xl font-bold text-blue-800">100+</p>
+              <p className="text-lg">Events Hosted</p>
+            </div>
+          </div>
+        </div>
+        <div className=" w-[457px] h[559px]">
+          <img className="rounded-lg" src={metting}></img>
+        </div>
+      </div>
+
+      {/* revolution workspace */}
+      <div className="bg-gray-900 py-24 rounded-lg mb-20">
+        <div className="w-[572px] text-center text-white mx-auto ">
+          <h1 className="text-4xl font-bold">
+            {" "}
+            Ready to Revolutionize Your Workspace?
+          </h1>
+          <p className="text-xl font-semibold my-10">
+            Request a quote or schedule a tour today!
+          </p>
+          <button className="btn  bg-indigo-900 text-[16px] text-white w-[138px] py-6 mr-4">
+            Enquire Now
+          </button>
         </div>
       </div>
     </div>
--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,9 +1,97 @@
 import React from "react";
+import { GoMail } from "react-icons/go";
+import { IoIosCall } from "react-icons/io";
+import { MdMessage } from "react-icons/md";
 
 const Contact = () => {
   return (
-    <div>
-      <h2>this is conatct</h2>
+    <div className="">
+      {/* title */}
+      <div className="w-[596px] mx-auto text-center mt-20">
+        <h1 className="text-5xl font-bold mb-6">Let's get in touch!</h1>
+        <p className="text-lg ">
+          Whether you're in the heart of Silicon Valley or New York City, find
+          the perfect backdrop for your business to thrive.
+        </p>
+      </div>
+
+      {/* submit  */}
+      <div
+        className="w-[800px] relative z-10 top-20  mx-auto bg-slate-100
+       shadow px-12 py-16 space-y-3 rounded"
+      >
+        <div className="grid grid-cols-2">
+          <fieldset className="fieldset ">
+            <legend className="fieldset-legend">Name</legend>
+            <input
+              type="text"
+              className="input w-[340px] h-[56px] px-4 "
+              placeholder="Type your name"
+            />
+          </fieldset>
+          <fieldset className="fieldset ">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="text"
+              className="input w-[340px] h-[56px] px-4 "
+              placeholder="Type your email"
+            />
+          </fieldset>
+          <fieldset className="fieldset ">
+            <legend className="fieldset-legend">Phone Number</legend>
+            <input
+              type="number"
+              className="input w-[340px] h-[56px] px-4 "
+              placeholder="Type your number"
+            />
+          </fieldset>
+          <fieldset className="fieldset ">
+            <legend className="fieldset-legend">Subject</legend>
+            <input
+              type="text"
+              className="input w-[340px] h-[56px] px-4 "
+              placeholder=""
+            />
+          </fieldset>
+        </div>
+        <fieldset className="fieldset ">
+          <legend className="fieldset-legend">Message</legend>
+          <textarea className="textarea h-52 w-full" placeholder=""></textarea>
+        </fieldset>
+        <button className="btn  bg-indigo-900 text-[18px] text-white  py-7 mr-4">
+          Submit Message
+        </button>
+      </div>
+
+      {/* phone/email/live chat */}
+      <div className="bg-gray-900 grid grid-cols-3 p-30 mb-20">
+        <div className="text-white border-r  p-8 space-y-4">
+          <span className="text-4xl">
+            <IoIosCall />
+          </span>
+          <h4 className="text-xl font-bold">Phone</h4>
+          <p>Give us a call to speak directly with our team.</p>
+          <h3 className="text-2xl text-blue-800 font-bold">(123) 456 - 7891</h3>
+        </div>
+        <div className="text-white border-r p-5 space-y-4">
+          <span className="text-4xl">
+            <MdMessage />
+          </span>
+          <h4 className="text-xl font-bold">Message</h4>
+          <p>Shoot us an email for prompt, detailed responses.</p>
+          <h3 className="text-2xl text-blue-800 font-bold">
+            email@example.com
+          </h3>
+        </div>
+        <div className="text-white  p-5 space-y-4">
+          <span className="text-4xl">
+            <GoMail className="text-white" />
+          </span>
+          <h4 className="text-xl font-bold">Live Chat</h4>
+          <p>Need instant answers? Use the live for real-time support.</p>
+          <h3 className="text-2xl text-blue-800 font-bold">Chat with us</h3>
+        </div>
+      </div>
     </div>
   );
 };
